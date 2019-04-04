12219f7bdbb9304bc0632a32badb5bbdf4c324cc
Mateo Neira <mneira@turing.ac.uk>
2019-04-04 17:34:11 +0100

    updated hex

--- js/map.js
+++ js/map.js
@@ -2,8 +2,10 @@ var rent_slidervar = document.getElementById('rent_slider');
 var travel_slidervar = document.getElementById('travel_slider');
 var rent_value = 800
 var travel_time = 90
-var data = []
+var hex_data = []
+var brownfield_data = []
 var mymap = L.map('map', {zoomControl:false}).setView([51.505, -0.09], 11);
+var brownfields = L.layerGroup().addTo(mymap);
 var polygons = L.layerGroup().addTo(mymap);
 var rent_property = "Studio"
 
@@ -25,30 +27,39 @@ noUiSlider.create(travel_slider, {
 
 rent_slidervar.noUiSlider.on('update', function(values, handle){
 	rent_value = values[0]
-    document.getElementById('rent_value').innerHTML =  Math.round(rent_value);
+    document.getElementById('rent_value').innerHTML =  Math.round(rent_value) + ' £';
 	//clear layers
 	polygons.clearLayers();
-	L.geoJSON(data, {style: style}).addTo(polygons);
+	L.geoJSON(hex_data, {style: style}).addTo(polygons);
 	//repopulate
 
 })
 
 travel_slidervar.noUiSlider.on('update', function(values, handle){
     travel_time = values[0]
-    document.getElementById('travel_value').innerHTML =  Math.round(travel_time);
+    document.getElementById('travel_value').innerHTML =  Math.round(travel_time) + ' min';
     // clear layers
     polygons.clearLayers();
-    L.geoJSON(data, {style: style}).addTo(polygons);
+    L.geoJSON(hex_data, {style: style}).addTo(polygons);
     //repopulate
 
 })
 
 // read json and add to map
-$.getJSON("https://raw.githubusercontent.com/mateoneira/interactive_map/2c0ca5328f3e3c499f9b017417101d1e7bae142d/data/london_rent.geojson", function(json) {
-    console.log(json); // this will show the info it in firebug console
-    data = json
-    L.geoJSON(data, {style: style}).addTo(polygons);
+$.getJSON("https://raw.githubusercontent.com/mateoneira/interactive_map/db1e64c941b8f24a33d3a6f859dba2376225c457/data/brownfield.geojson", function(json) {
+    // console.log(json); // this will show the info it in firebug console
+    brownfield_data = json
+    L.geoJSON(brownfield_data, {style: {fillColor: '#0f0f0f', fillOpacity:0.5, weight:1, color:'#000000'}}).addTo(brownfields);
 });
+
+
+// // read json and add to map
+$.getJSON("https://raw.githubusercontent.com/mateoneira/interactive_map/2c0ca5328f3e3c499f9b017417101d1e7bae142d/data/london_rent.geojson", function(json) {
+    // console.log(json); // this will show the info it in firebug console
+    hex_data = json
+    L.geoJSON(hex_data, {style: style}).addTo(polygons);
+});
+
 
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -84,6 +95,6 @@ $("input[type='radio']").click(function(){
     if(radioValue){
         rent_property = radioValue
         polygons.clearLayers();
-		L.geoJSON(data, {style: style}).addTo(polygons);
+		L.geoJSON(hex_data, {style: style}).addTo(polygons);
     }
 });
